f92a642da1f31133a6aa096c0cb892d9c6992bff
tsekiguc <tsekiguc@student.42tokyo.jp>
2023-04-11 22:15:56 +0900

    chore: ログイン機能追加

---
+++ frontend/src/hooks/useAuth.ts
@@ -0,0 +1,37 @@
+import { useCallback, useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
+import { type User } from '../types/api/user';
+
+export const useAuth = (): {
+  login: (id: string) => void;
+  loading: boolean;
+} => {
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+
+  const login = useCallback(
+    (id: string) => {
+      setLoading(true);
+      axios
+        .get<User>(`https://jsonplaceholder.typicode.com/users/${id}`)
+        .then((res) => {
+          if (res.data != null) {
+            navigate('/home');
+          } else {
+            alert('ユーザーが見つかりません');
+          }
+        })
+        .catch(() => {
+          alert('ログインできません');
+        })
+        .finally(() => {
+          setLoading(false);
+        });
+    },
+    [navigate, setLoading]
+  );
+
+  return { login, loading };
+};
