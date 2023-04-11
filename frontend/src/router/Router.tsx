2404d6eed84f1e3cbd55f37601a6615942a1126d
tsekiguc <tsekiguc@student.42tokyo.jp>
2023-04-11 16:20:56 +0900

    chore: Navigateを使うように修正。

--- frontend/src/router/Router.tsx
+++ frontend/src/router/Router.tsx
@@ -1,4 +1,5 @@
 import { memo, type VFC } from 'react';
+import { Navigate } from 'react-router';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import { Page404 } from 'components/pages/Page404';
@@ -26,7 +27,7 @@ export const Router: VFC = memo(() => {
           }
         />
         <Route path="/" element={<Login />} />
-        <Route path="*" element={<Page404 />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
   );
