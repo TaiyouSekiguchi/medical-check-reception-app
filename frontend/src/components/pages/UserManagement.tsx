e2aeda09220d3f69985838fe29c5fe3d88ef163f
tsekiguc <tsekiguc@student.42tokyo.jp>
2023-04-12 13:26:32 +0900

    chore: ユーザー一覧取得

--- frontend/src/components/pages/UserManagement.tsx
+++ frontend/src/components/pages/UserManagement.tsx
@@ -1,17 +1,35 @@
-import { memo, type VFC } from 'react';
-import { Wrap, WrapItem } from '@chakra-ui/react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import { memo, useEffect, type VFC } from 'react';
+import { Center, Spinner, Wrap, WrapItem } from '@chakra-ui/react';
+import { useAllUsers } from '../../hooks/useAllUsers';
 import { UserCard } from '../organisms/user/UserCard';
 
 export const UserManagement: VFC = memo(() => {
+  const { getUsers, loading, users } = useAllUsers();
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
   return (
-    <Wrap p={{ base: 4, md: 10 }}>
-      <WrapItem>
-        <UserCard
-          imageUrl="https://source.unsplash.com/random"
-          userName="じゃけぇ"
-          fullName="Takumi Okada"
-        />
-      </WrapItem>
-    </Wrap>
+    <>
+      {loading ? (
+        <Center h="100vh">
+          <Spinner />
+        </Center>
+      ) : (
+        <Wrap p={{ base: 4, md: 10 }} justify="center">
+          {users.map((user) => (
+            <WrapItem key={user.id} mx="auto">
+              <UserCard
+                imageUrl="https://source.unsplash.com/random"
+                userName={user.username}
+                fullName={user.name}
+              />
+            </WrapItem>
+          ))}
+        </Wrap>
+      )}
+    </>
   );
 });
