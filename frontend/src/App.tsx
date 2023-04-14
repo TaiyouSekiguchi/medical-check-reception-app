d428a7448facb8ac205fb4d5b3849edaaf212172
tsekiguc <tsekiguc@student.42tokyo.jp>
2023-04-14 14:33:24 +0900

    chore: CSRF Tokenの設定

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,24 @@
+import { useEffect } from 'react';
+import { axios } from 'lib/axios';
+import { type CsrfToken } from 'types';
 import { AppProvider } from 'providers/AppProvider';
 import { Router } from 'router/Router';
 
 const App = (): JSX.Element => {
+  useEffect(() => {
+    const getCsrfToken = async () => {
+      const { data } = await axios.get<CsrfToken>(`/csrf`);
+      axios.defaults.headers.common['X-CSRF-Token'] = data.csrf_token;
+    };
+    getCsrfToken()
+      .then(() => {
+        console.log('Successfully get csrf token');
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <AppProvider>
       <Router />
