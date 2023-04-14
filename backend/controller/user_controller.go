488e24e0c65553c7dc3078abfd84bff6f8a8f014
tsekiguc <tsekiguc@student.42tokyo.jp>
2023-04-14 22:06:56 +0900

    chore: signup機能不要なので削除

--- backend/controller/user_controller.go
+++ backend/controller/user_controller.go
@@ -11,7 +11,6 @@ import (
 )
 
 type IUserController interface {
-	SignUp(c echo.Context) error
 	LogIn(c echo.Context) error
 	LogOut(c echo.Context) error
 	CsrfToken(c echo.Context) error
@@ -23,18 +22,6 @@ type userController struct {
 
 func NewUserController(uu usecase.IUserUsecase) IUserController {
 	return &userController{uu}
-}
-
-func (uc *userController) SignUp(c echo.Context) error {
-	user := model.User{}
-	if err := c.Bind(&user); err != nil {
-		return c.JSON(http.StatusBadRequest, err.Error())
-	}
-	userRes, err := uc.uu.SignUp(user)
-	if err != nil {
-		return c.JSON(http.StatusInternalServerError, err.Error())
-	}
-	return c.JSON(http.StatusCreated, userRes)
 }
 
 func (uc *userController) LogIn(c echo.Context) error {
