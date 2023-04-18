cffdfb1a1121dd693a5d16f4569ea607408b34df
tsekiguc <tsekiguc@student.42tokyo.jp>
2023-04-18 10:38:02 +0900

    feat: 予約枠一覧ページ作成

--- frontend/src/router/Router.tsx
+++ frontend/src/router/Router.tsx
@@ -3,11 +3,12 @@ import { Navigate } from 'react-router';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import { Page404 } from 'components/pages/Page404';
+import { ReservationSlotList } from 'components/pages/ReservationSlotList';
 import { UserManagement } from 'components/pages/UserManagement';
 import { HeaderLayout } from 'components/templates/HeaderLayout';
 import { LoginUserProvider } from 'providers/LoginUserProvider';
 import { Home } from '../components/pages/Home';
-import { InsuredList } from '../components/pages/Insureds';
+import { InsuredList } from '../components/pages/InsuredList';
 import { Login } from '../components/pages/Login';
 import { Setting } from '../components/pages/Setting';
 
@@ -24,6 +25,10 @@ export const Router: VFC = memo(() => {
                   <Route path="" element={<Home />} />
                   <Route path="setting" element={<Setting />} />
                   <Route path="insured_list" element={<InsuredList />} />
+                  <Route
+                    path="reservation_slot_list"
+                    element={<ReservationSlotList />}
+                  />
                   <Route path="user_management" element={<UserManagement />} />
                   <Route path="*" element={<Page404 />} />
                 </Routes>
---
+++ frontend/src/types/api/reservationSlot.ts
@@ -0,0 +1,8 @@
+export type ReservationSlot = {
+  id: number;
+  date: string;
+  basic: number;
+  gastrointestinal_endoscopy: number;
+  barium: number;
+  breast_cancer_screening: number;
+};
