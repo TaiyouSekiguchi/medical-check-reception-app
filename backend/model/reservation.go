86d47f071c01ce3bc56bc5a59e8cf79141c6aa6a
tsekiguc <tsekiguc@student.42tokyo.jp>
2023-04-19 15:34:14 +0900

    feat: 予約用テーブル作成

---
+++ backend/model/reservation.go
@@ -0,0 +1,15 @@
+package model
+
+import "time"
+
+type Reservation struct {
+	ID                uint            `json:"id" gorm:"primaryKey"`
+	InsuredID         uint            `json:"insured_id" gorm:"not null"`
+	Insured           Insured         `json:"insured" gorm:"references:ID"`
+	ReservationSlotID uint            `json:"reservation_slot_id" gorm:"not null"`
+	ReservationSlot   ReservationSlot `json:"reservation_slot" gorm:"references:ID"`
+	ExaminationItemID uint            `json:"examination_item_id" gorm:"not null"`
+	ExaminationItem   ExaminationItem `json:"examination_item" gorm:"references:ID"`
+	CreatedAt         time.Time       `json:"created_at"`
+	UpdatedAt         time.Time       `json:"updated_at"`
+}
