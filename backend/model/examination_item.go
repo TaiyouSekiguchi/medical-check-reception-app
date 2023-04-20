2e5ba4d6fee5438233301c7a78699d503621974c
tsekiguc <tsekiguc@student.42tokyo.jp>
2023-04-21 00:27:09 +0900

    chore: ExaminationItem定義修正

--- backend/model/examination_item.go
+++ backend/model/examination_item.go
@@ -2,10 +2,10 @@ package model
 
 type ExaminationItem struct {
 	ID   uint   `json:"id" gorm:"primary_key"`
-	Name string `json:"name" gorm:"unique"`
+	Name string `json:"name" gorm:"unique; not null; type:varchar(255)"`
 }
 
 type ExaminationItemResponse struct {
 	ID   uint   `json:"id" gorm:"primary_key"`
-	Name string `json:"name" gorm:"unique"`
+	Name string `json:"name" gorm:"unique; not null; type:varchar(255)"`
 }
