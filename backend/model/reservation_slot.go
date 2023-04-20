a848ec1d4c3e737996fc0c3a8e0143a0cd5bb9ba
tsekiguc <tsekiguc@student.42tokyo.jp>
2023-04-21 00:31:56 +0900

    chore: ReservationSlot定義修正

--- backend/model/reservation_slot.go
+++ backend/model/reservation_slot.go
@@ -4,11 +4,11 @@ import "time"
 
 type ReservationSlot struct {
 	ID                        uint          `json:"id" gorm:"primaryKey"`
-	Date                      time.Time     `json:"date"`
-	Basic                     uint          `json:"basic"`
-	GastrointestinalEndoscopy uint          `json:"gastrointestinal_endoscopy"`
-	Barium                    uint          `json:"barium"`
-	BreastCancerScreening     uint          `json:"breast_cancer_screening"`
+	Date                      time.Time     `json:"date" gorm:"not null"`
+	Basic                     uint          `json:"basic" gorm:"not null"`
+	GastrointestinalEndoscopy uint          `json:"gastrointestinal_endoscopy" gorm:"not null"`
+	Barium                    uint          `json:"barium" gorm:"not null"`
+	BreastCancerScreening     uint          `json:"breast_cancer_screening" gorm:"not null"`
 	CreatedAt                 time.Time     `json:"created_at"`
 	UpdatedAt                 time.Time     `json:"updated_at"`
 	Reservation               []Reservation `json:"reservation" gorm:"foreignKey:ReservationSlotID"`
@@ -16,10 +16,12 @@ type ReservationSlot struct {
 
 type ReservationSlotResponse struct {
 	ID                        uint                  `json:"id" gorm:"primaryKey"`
-	Date                      time.Time             `json:"date"`
-	Basic                     uint                  `json:"basic"`
-	GastrointestinalEndoscopy uint                  `json:"gastrointestinal_endoscopy"`
-	Barium                    uint                  `json:"barium"`
-	BreastCancerScreening     uint                  `json:"breast_cancer_screening"`
+	Date                      time.Time             `json:"date" gorm:"not null"`
+	Basic                     uint                  `json:"basic" gorm:"not null"`
+	GastrointestinalEndoscopy uint                  `json:"gastrointestinal_endoscopy" gorm:"not null"`
+	Barium                    uint                  `json:"barium" gorm:"not null"`
+	BreastCancerScreening     uint                  `json:"breast_cancer_screening" gorm:"not null"`
+	CreatedAt                 time.Time             `json:"created_at"`
+	UpdatedAt                 time.Time             `json:"updated_at"`
 	Reservation               []ReservationResponse `json:"reservation"`
 }
