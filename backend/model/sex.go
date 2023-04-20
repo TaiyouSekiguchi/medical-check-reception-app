87c80871910d1944a4b5d0105e52d0ae4b0dee77
tsekiguc <tsekiguc@student.42tokyo.jp>
2023-04-21 00:24:36 +0900

    chore: Sex定義修正

--- backend/model/sex.go
+++ backend/model/sex.go
@@ -2,6 +2,6 @@ package model
 
 type Sex struct {
 	ID   uint   `json:"id" gorm:"primary_key"`
-	Name string `json:"name" gorm:"unique;not null"`
+	Name string `json:"name" gorm:"unique;not null; type:varchar(255)"`
 	Code uint   `json:"code" gorm:"unique;not null"`
 }
